578d7352de7ce4537030f195f8cb0ed4adf91513
alexander <ahansen1335@gmail.com>
2023-11-26 11:18:17 +0100

    create temporary renaming scheme with send2trash commented out for testing purposes

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ def main():
     original_file = input('PDF file name to split: ')
     reader = PdfReader(original_file)
     split.split(reader)
-    send2trash(original_file)
+    # send2trash(original_file)
+    filenames = os.listdir(working_directory)
+    TEMP = ['A', 'B', 'C', 'D', 'E', 'F']
+    i = 0
+    for filename in filenames:
+        if filename.endswith('.pdf'):
+            os.rename(filename, TEMP[i] + '.pdf')  
+            i += 1   
 
-            
 
 if __name__ == '__main__':
     main()
