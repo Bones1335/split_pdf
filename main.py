d2c39bc01d06a3dd15310d817deeb579a178dfdb
alexander <ahansen1335@gmail.com>
2023-11-26 07:43:17 +0100

    write main function to split off one page, but saves to program's directory

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from pypdf import PdfReader
+from pypdf import PdfReader, PdfWriter
 
 def main():
     reader = PdfReader(input('Input file path to desired pdf to split: '))
-    print(reader)
+    writer = PdfWriter()
+    writer.add_page(reader.pages[2])
+    with open(input('File name: '), 'wb') as output_stream:
+        writer.write(output_stream)
+
+    # print(page.extract_text())
 
 if __name__ == '__main__':
     main()
