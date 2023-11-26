9c26b7dc14b9283408d1c46f9692f9fdace16fc2
alexander <ahansen1335@gmail.com>
2023-11-26 10:43:45 +0100

    add send2trash function for original file

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
-from pypdf import PdfReader, PdfWriter
+from pypdf import PdfReader
+from send2trash import send2trash
 import split
 
 def main():
     working_directory = input('Input directory path to desired pdf to split: ')
     os.chdir(working_directory)
 
-    reader = PdfReader(input('PDF file name to split: '))
+    original_file = input('PDF file name to split: ')
+    reader = PdfReader(original_file)
     split.split(reader)
+    send2trash(original_file)
+
             
 
 if __name__ == '__main__':
