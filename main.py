8af4904c90c62f9636f9a2ceaba8ab7c16b3c36e
alexander <ahansen1335@gmail.com>
2023-11-26 10:11:50 +0100

    split pdf file into multiple pdf files creating a new file every 4th page

--- main.py
+++ main.py
@@ -6,12 +6,23 @@ def main():
     os.chdir(working_directory)
 
     reader = PdfReader(input('PDF file name to split: '))
-    writer = PdfWriter()
-    writer.add_page(reader.pages[2])
-    with open(input('New file name: '), 'wb') as output_stream:
-        writer.write(output_stream)
+    
+    start = 0
+    end = 4
 
-    # print(page.extract_text())
+    for page in range(int(len(reader.pages) / 2)):
+        writer = PdfWriter()
+        new_file_name = str(page + 1) + '.pdf'
+        for i in range(start,end):
+            if i == len(reader.pages) or i > len(reader.pages):
+                break
+            writer.add_page(reader.pages[i])          
+            with open (new_file_name, 'wb') as out:
+                writer.write(out)
+
+        start = end
+        end = start + 4
+            
 
 if __name__ == '__main__':
     main()
