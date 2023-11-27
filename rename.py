c997d3f475195cab143b21814469363c4852c48b
alexander <ahansen1335@gmail.com>
2023-11-27 07:22:23 +0100

    add csv file check to rename.py to check if a csv file exists in the directory

--- rename.py
+++ rename.py
@@ -2,16 +2,19 @@ import csv
 import os
 
 def rename(filenames):
-    
-    with open('names.csv', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        i = 0
-        for row in reader:
-            name = row['NOM'] + '_' + row['Prénom']
-            if i >= len(filenames):
-                break
-            file = filenames[i]
-            if file.endswith('.pdf'):    
-                filenames[i] = os.rename(filenames[i], name + '.pdf')
-            i += 1
+    file = 'names.csv'
+    if file in filenames:
+        with open(file, encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            i = 0
+            for row in reader:
+                name = row['NOM'] + '_' + row['Prénom']
+                if i >= len(filenames):
+                    break
+                file = filenames[i]
+                if file.endswith('.pdf'):    
+                    filenames[i] = os.rename(filenames[i], name + '.pdf')
+                i += 1
+    else:
+        print('There is no CSV file to rename files with.')
               
