7dedf1cc383688a92b34bed0c64dfed95a66d611
alexander <ahansen1335@gmail.com>
2023-12-28 14:51:26 +0100

    refactor rename.py to use enumerate in the for loop to make the program more pythonic

--- rename.py
+++ rename.py
@@ -6,15 +6,13 @@ def rename(filenames):
     if file in filenames:
         with open(file, encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile, fieldnames=['NOM', 'Prénom'])
-            i = 0
-            for row in reader:
+            for i, row in enumerate(reader):
                 name = row['NOM'] + '_' + row['Prénom']
                 if i >= len(filenames):
                     break
                 file = filenames[i]
                 if file.endswith('.pdf'):    
                     filenames[i] = os.rename(filenames[i], name + '.pdf')
-                i += 1
     else:
         print('There is no CSV file to rename files with.')
               
